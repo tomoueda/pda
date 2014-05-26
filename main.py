1ea77e8301248eca9c6609c1e294ca465fff17e8
Tomo Ueda <tueda14@gmail.com>
2014-05-25 22:17:51 -0700

    finished the initial project could add many more features, now to transport it to the web.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import urllib2
 import fileinput
 import re
 import json
+import operator
+from collections import Counter
 from lxml import etree
 
 #this corresponds to the main site we are scraping data off of.
@@ -12,7 +14,58 @@ domain = 'http://www.puzzledragonx.com/'
 start_domain = 'http://www.puzzledragonx.com/?dir=1'
 grand_parties = {}
 associated_parties = {}
-non_decimal = re.compile(r'[^\d.]+')
+non_decimal = re.compile(r'[^\d]+')
+
+class MonsterData:
+    """
+    The constructor of the monster data,
+    takes in monster number.
+    """
+    def __init__(self, monster_num):
+        self.monster_num = monster_num
+        self.synergy = Counter()
+        self.dungeons = Counter()
+        self.dungeon_ratio = {}
+        self.leader = 0
+        self.friend = 0
+        self.sub = 0
+        self.appearance = 0
+
+    def add_one_leader(self):
+        self.leader += 1
+
+    def add_one_friend(self):
+        self.friend += 1
+
+    def add_one_sub(self):
+        self.sub += 1
+
+    def add_one(self):
+        self.appearance += 1
+
+    def add_one_teammate(self, monster_id):
+        self.synergy[monster_id] += 1
+
+    def add_one_dungeon(self, dungeon):
+        self.dungeons[dungeon] += 1
+
+    def get_topx_teammate(self, x):
+        return self.synergy.most_common(x)
+
+    def get_topx_dungeons(self, x):
+        return self.dungeons.most_common(x)
+
+    def __str__(self):
+        best_dungeon = max(self.dungeon_ratio.iteritems(), key=operator.itemgetter(1))[0]
+        return str('monster id:' + str(self.monster_num) + '\n' +
+                   'top ten monster friends: ' + str(self.synergy.most_common(10)) + '\n' + 
+                   'top ten dungeons: ' + str(self.dungeons.most_common(10)) + '\n' +
+                   'best dungeon ratio: ' + str(best_dungeon) + \
+                   ' with ratio:' + str(self.dungeon_ratio[best_dungeon]) + '\n' + 
+                   'leader:' + str(self.leader) + '\n' + 
+                   'friend:' + str(self.friend) + '\n' +
+                   'sub:' + str(self.sub) + '\n' +
+                   'appearances:' + str(self.appearance) + '\n')
 
 def main():
     dungeon_urls = get_dungeon_urls()
@@ -20,46 +73,75 @@ def main():
     for line in fileinput.input():
         line = non_decimal.sub('', line)
         monster_num = int(line)
-        find_associations(dungeon_urls, monster_num)
+        monster = MonsterData(monster_num)
+        find_associations(dungeon_urls, monster)
+        print monster
+
 
 """
 Given a list of dungeon urls and a monster number
 we could find all the parties this monster is in.
 """
-def find_associations(dungeon_urls, monster_num):
+def find_associations(dungeon_urls, monster):
     for url in dungeon_urls:
         str_url = domain + url.attrib["href"]
         response = urllib2.urlopen(str_url)
         root = etree.parse(response, etree.HTMLParser())
         different_difficulties = root.findall("//td[@class=\"title nowrap\"]/a")
-        print_associations(root, url, monster_num)
+        print_associations(root, url, monster)
         for durl in different_difficulties:
             request =  domain + 'en/' + durl.attrib["href"]
             response = urllib2.urlopen(request)
             root_in = etree.parse(response, etree.HTMLParser())
             # parties = root_in.findall("//td[@class=\"pt\"]")
             # grand_parties[durl.text] = parties
-            print_associations(root_in, durl, monster_num)
+            print_associations(root_in, durl, monster)
 
 """
 Given a root html, the url and the monster number
 we print out all parties associated with current dungeon
-that has the monster in question.
+that has the monster in question. Also updates monster
+information
 """
-def print_associations(root, url, monster_num):
+def print_associations(root, url, monster):
+    dungeon_counter = 0
     parties = root.findall("//td[@class=\"pt\"]")
     grand_parties[url.text] = parties
-    assoc_parties = get_assoc(monster_num, parties)
+    assoc_parties = get_assoc(monster.monster_num, parties)
     if assoc_parties:
         print '========================================='
         print url.text
         for party in assoc_parties:
+            dungeon_counter += 1
+            monster.add_one_dungeon(url.text)
+            placement = 0 # to figure out placement
             for elem in party.iter():
                 if 'href' in elem.attrib:
                     image = elem.find("img")
                     if (image != None):
                         print image.attrib['title']
+                        idnum = int(non_decimal.sub('',image.attrib['title']))
+                        update_monster_info(placement, idnum, monster)
+                        placement += 1
             print '======================'
+        monster.dungeon_ratio[url.text] = float(float(dungeon_counter) / float(len(parties)))
+
+"""
+updates the monster info according to the placement of 
+the monster in the team and other monsters that are
+teamed up with this monster.
+"""
+def update_monster_info(placement, idnum, monster):
+    if idnum == monster.monster_num:
+        monster.add_one()
+        if placement == 0:
+            monster.add_one_leader()
+        elif placement == 5:
+            monster.add_one_friend()
+        else:
+            monster.add_one_sub()
+    else:
+        monster.add_one_teammate(idnum)
 
 
 """
